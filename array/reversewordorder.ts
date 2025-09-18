05104091d45ca15e7f56a87bc43f491b8c449cf7
kyalok <kyalok@co-opbank.co.ke>
2025-09-18 17:17:45 +0300

    Started reverse word order

---
+++ array/reversewordorder.ts
@@ -0,0 +1,12 @@
+export function reverseWordOrder(s: string): string {
+    const strArray: string[] = s.split(' ')
+    let left: number = 0
+    let right: number = strArray.length - 1
+    while(left < right) {
+        
+    }
+    console.log(strArray)
+    return "test"
+}
+
+console.log(reverseWordOrder("This is a    string"))
